ed31aadf6040ae5746dd67223528d99ce08cfd3c
nambe1 <nambe1@student.op.ac.nz>
2020-04-01 15:18:33 +1300

    ....

--- sketch_mar28a/sketch_mar28a.ino
+++ sketch_mar28a/sketch_mar28a.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include "Countimer.h"
+
 int redPin = 3;
 int bluePin = 5;
 int greenPin = 6;
@@ -12,14 +14,34 @@ void setup() {
     myServo.attach(9);
     myServo.write(10);
 
-      /* This is the Alcohol sensor*/
+   /* This is the Alcohol sensor*/
 
     pinMode(MQ3, INPUT);
+
+  /*This is a timer.*/
+  timer.setCounter(0, 0, 10, timer.COUNT_DOWN, onComplete);
+  timer.setInterval(refreshClock,1000);
+    
 
 
 }
 
+void redreshClock(){
+  Serial.print("Current time:")
+  Serial.printlm(timer.getCurrenttime());
+  /*Will need to be modified.*/
+}
+
+void onComplete()
+{
+  Serial.println("Time out.");
+}
+}
 void loop() {
+
+  //running timer.
+  timer.run();
+  
   // put your main code here, to run repeatedly:
     
     
@@ -44,10 +66,13 @@ void loop() {
     delay(100);
      
     }
-
        analogWrite(redPin,50);
        analogWrite(bluePin,50);
        analogWrite(greenPin,50);
-    
+       
+    if(!timer.isCounterCompleter())
+    {
+      timer.start();
+    }
 
 }
