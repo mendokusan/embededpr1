b35a37cab2be1d52bef62a98d5a8fd266c81b7fb
marrnj2 <marrnj2@student.op.ac.nz>
2020-04-05 18:47:20 +1200

    Final COmmit

--- sketch_mar28a/sketch_mar28a.ino
+++ sketch_mar28a/sketch_mar28a.ino
@@ -1,89 +1,157 @@
 #include <Servo.h>
+#include <Chrono.h>
 
 int redPin = 3;
 int bluePin = 5;
 int greenPin = 6;
 int MQ3 = A0;
 int value;
+int btnPin = 2;
+Servo myServo;
+Chrono timer(Chrono::SECONDS);
+
+int buzzerPin = 11;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   /* Controls the servo */
-    Servo myServo;
+   
     myServo.attach(9);
-    myServo.write(10);
-
-   /* This is the Alcohol sensor*/
-
+    pinMode(btnPin,  INPUT_PULLUP);
     pinMode(MQ3, INPUT);
+    pinMode(buzzerPin, OUTPUT);
 
-  /*This is a timer.*/    
 
-
-}
 
 
 }
 void loop() {
 
-  //running timer.
-  
-  
-  // put your main code here, to run repeatedly:
-    
-    
+  // put your main code here, to run repeatedly:    
     // This is the amount of alcohol detected from the sensor
-  value = analogRead(MQ3);
-
    // This will light the led if alcohol is present
-    Serial.println(value);
-    
-      //if(value > 400)
-      //{
-        //analogWrite(redPin,100);
-        //analogWrite(bluePin,100);
-        //analogWrite(greenPin,100);
-      //}
-      //else{
-         //analogWrite(redPin,0);
-        //analogWrite(bluePin,0);
-        //analogWrite(greenPin,0);    
-        //}
-    //delay(100);
-
-      //t.every(1000, takeReading); 
-    
-       //analogWrite(redPin,50);
-       //analogWrite(bluePin,50);
-       //analogWrite(greenPin,50);
-
-       if(value>350)
-       {
-        failedTest();
-       }
-       else{
-        passedTest();
-        }
-
+   DetectStart();
+ 
+}
+void DetectStart()
+{
+  if(digitalRead(btnPin) == LOW)
+ {
+     ClearLightColor();
+     FirstTest();
+ }
 }
 
-void failedTest()
+bool FirstTest()
 {
+  int result = DeteectAlcohol();
+  if(result > 400){
+    FailedState();
+  }
+  else{
+    ClearLightColor();
+    GreenLight();
+    UnlockCar();
+    PassedTone();
+  }
+ 
+}
 
-   analogWrite(redPin,100);
+int DeteectAlcohol(){
+  int maxContent = 0;
+  for(int i = 0; i < 10; i++)
+  {
+      Proccesing();
+      value = analogRead(MQ3);
+       Serial.println(value);
+      delay(1000);
+      if(value > maxContent )
+      {
+       maxContent = value;
+      }
+  }
+
+  return maxContent;
+}
+
+void FailedState()
+{
+  timer.restart();
+  LockCar();
+  FailedTone();
+  RedLight();
+  while(timer.hasPassed(20) == false)
+  {
+    if(timer.elapsed() % 2 == 0)
+    {
+      analogWrite(redPin,255);
+    }
+      analogWrite(redPin,0);
+  
+}
+    timer.stop();
+    GreenLight();
+    AzureLight();
+    DetectStart();
+}
+void RedLight()
+{
+   analogWrite(redPin,80);
    analogWrite(bluePin,0);
    analogWrite(greenPin,0);
-   delay(100);
-   passedTest();
 }
 
-void passedTest()
+void GreenLight()
 {
 
-   analogWrite(redPin,0);
-   analogWrite(bluePin,0);
    analogWrite(greenPin,100);
-   delay(100);
-   
-   
+}
+void Proccesing()
+{
+    analogWrite(redPin,0);
+    analogWrite(greenPin,0);
+    delay(10);
+    analogWrite(redPin,255);
+    analogWrite(greenPin,100);
+ 
+
+}
+void AzureLight()
+{
+   analogWrite(redPin,255);
+   analogWrite(bluePin,255);
+   analogWrite(greenPin,255);
+}
+void ClearLightColor()
+{
+  analogWrite(redPin,0);
+   analogWrite(bluePin,0);
+   analogWrite(greenPin,0);
+}
+
+void FailedTone(){
+  
+    tone(buzzerPin, 200); 
+    delay(150);
+    tone(buzzerPin, 255); 
+    delay(150);
+    tone(buzzerPin, 200);
+    delay(150);
+    noTone(buzzerPin);
+     
+  }
+void PassedTone(){
+    tone(buzzerPin, 50); 
+    delay(150);
+    tone(buzzerPin, 100); 
+    delay(150);
+    tone(buzzerPin, 200);
+    delay(150);
+    noTone(buzzerPin);
+}
+void LockCar(){
+    myServo.write(255);
+}
+void UnlockCar(){
+  myServo.write(0);
 }
